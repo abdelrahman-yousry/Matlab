6f9cc53c817e8993ebca7964f2ec18e4f181558d
DESKTOP-8AQ5VUP\U3 <abdelrahman.yousry000@gmail.com>
2022-04-05 20:15:25 +0200

    switch to control led using subsystem function

---
+++ task_6_/switch_led_logic_ert_rtw/switch_led_logic_private.h
@@ -0,0 +1,25 @@
+/*
+ * File: switch_led_logic_private.h
+ *
+ * Code generated for Simulink model 'switch_led_logic'.
+ *
+ * Model version                  : 1.6
+ * Simulink Coder version         : 9.5 (R2021a) 14-Nov-2020
+ * C/C++ source code generated on : Tue Apr  5 14:09:57 2022
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: Intel->x86-64 (Windows64)
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#ifndef RTW_HEADER_switch_led_logic_private_h_
+#define RTW_HEADER_switch_led_logic_private_h_
+#include "rtwtypes.h"
+#endif                              /* RTW_HEADER_switch_led_logic_private_h_ */
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
---
+++ task_6_/switch_led_logic_ert_rtw/switch_led_logic_types.h
@@ -0,0 +1,30 @@
+/*
+ * File: switch_led_logic_types.h
+ *
+ * Code generated for Simulink model 'switch_led_logic'.
+ *
+ * Model version                  : 1.6
+ * Simulink Coder version         : 9.5 (R2021a) 14-Nov-2020
+ * C/C++ source code generated on : Tue Apr  5 14:09:57 2022
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: Intel->x86-64 (Windows64)
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#ifndef RTW_HEADER_switch_led_logic_types_h_
+#define RTW_HEADER_switch_led_logic_types_h_
+
+/* Model Code Variants */
+
+/* Forward declaration for rtModel */
+typedef struct tag_RTM_switch_led_logic_T RT_MODEL_switch_led_logic_T;
+
+#endif                                /* RTW_HEADER_switch_led_logic_types_h_ */
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
